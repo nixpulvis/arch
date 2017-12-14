a47ad8732b8d84f3b98b6be3d7bbd50307a48821
Nathan Lilienthal <nathan@nixpulvis.com>
2017-12-14 13:13:15 -0700

    First draft of the bootstrap script.

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -2,6 +2,7 @@
 
 usage() {
     echo "TODO"
+    exit $1
 }
 
 confirm() {
@@ -28,17 +29,42 @@ while getopts 'e:h' arg; do
 done
 shift $((OPTIND -1))
 
-TARGET=$1
+target=$1
+
+if [ -z "$target" ]; then
+    usage 1
+fi
 
 # Confirm the target.
-echo "Bootstrapping $TARGET, this will format the device."
+echo "Bootstrapping $target, this will format the device."
 
 # Warn users about erase time.
 if [ -n "$erase" ]; then
-    echo "Erasing $TARGET with $erase, this can take a while."
+    echo "Erasing $target with $erase, this can take a while."
 fi
 
 # Display some information about the target device.
-lsblk $TARGET 
+lsblk $target 
 
 confirm
+# From this point on we don't ask the user for anything.
+
+if [ -n "$erase" ]; then
+    dd if=$erase of=$target status=progress
+fi
+
+fdisk $target << EOF
+g
+n
+
+
++512M
+t
+1
+n
+
+
+
+p
+w
+EOF
