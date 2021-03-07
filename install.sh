bf0689f287a3df447e44f85a4ff6adbc7b0c8646
Nathan Lilienthal <nathan@nixpulvis.com>
2021-03-07 17:13:17 -0500

    Handle 'p' in partition names and small cleanup

--- install.sh
+++ install.sh
@@ -7,10 +7,29 @@ usage() {
     exit $1
 }
 
+error() {
+    echo "ERROR: $1"
+    exit 1
+}
+
+ending_digit() {
+    case $1 in
+        *[0-9]) true ;;
+        *) false ;;
+    esac
+}
+
+partition_name() {
+    if ending_digit $1; then
+        echo "$1p$2"
+    else
+        echo "$1$2"
+    fi
+}
+
 # Parse the command line arguments.
-while getopts 've:h' arg; do
-    case "${arg}" in
-        v) verbose=1 ;;
+while getopts 've:h' arg; do case "${arg}" in
+        v) verbose=true ;;
         e) erase="${OPTARG}" ;;
         h) usage 0 ;;
         *)
@@ -21,6 +40,8 @@ while getopts 've:h' arg; do
 done
 shift $((OPTIND -1))
 target=$1
+boot=$(partition_name $target 1)
+root=$(partition_name $target 2)
 
 confirm() {
     read -p "Are you sure? [Y/n] " answer
@@ -28,42 +49,42 @@ confirm() {
         [yY][eE][sS]|[yY])
 	    ;;
 	*)
-	    echo "Quitting..."
+	    echo "quitting."
 	    exit 0
 	    ;;
     esac
 }
 
+# Format, partitions and creates the file systems for a new installation.
+# This function can optionally wipe the old data by simply writing over it
+# all.
 bootstrap() {
-    # Confirm the target.
     echo "Bootstrapping $target, this will format the device."
-
-    # Warn users about erase time.
     if [ -n "$erase" ]; then
         echo "Erasing $target with $erase, this can take a while."
     fi
 
-    # Display some information about the target device.
     lsblk $target
-
     confirm
     echo
 
     # From this point on we don't ask the user for anything.
 
     # Remove all mounts of the target device.
-    umount $target?* 2>/dev/null || true
+    if umount $target?* 2>| grep -q 'target is busy'; then
+        error "could not unmount $target"
+    fi
 
     # TODO: Mount a plain crypt and wipe with that.
     if [ -n "$erase" ]; then
         dd if=$erase of=$target status=progress
     fi
 
-    echo $target
-
-    # Format the device.
-    # NOTE: Try to get the OS to ignore the old partitions.
+    # Try to get the OS to ignore the old partitions.
     partx -u $target
+
+    # Format the target with a GPT, 512MB EFI partition #1 and the rest
+    # for the root filesystem.
     fdisk $target << EOF
 g
 n
@@ -79,25 +100,26 @@ n
 p
 w
 EOF
-    cryptsetup luksFormat ${target}2
-    cryptsetup luksOpen ${target}2 cryptroot
+    cryptsetup luksFormat $root
+    cryptsetup luksOpen $root cryptroot
 
     # Setup the filesystems.
-    mkfs.vfat -F32 ${target}1
+    mkfs.vfat -F32 $boot
     mkfs.ext4 /dev/mapper/cryptroot
 }
 
+# Installs an updated Arch to the formatted target
 install() {
     mkdir -p mnt
     mount /dev/mapper/cryptroot mnt
     mkdir -p mnt/boot
-    mount ${target}1 mnt/boot
+    mount $boot mnt/boot
 
     # TODO: Check for network.
     # TODO: Check host locale settings.
 
     # Install Arch (requires network connection).
-    cat packages.txt | xargs pacstrap mnt base
+    cat packages.txt | xargs pacstrap mnt
 
     # Configure fstab for the new install to correctly mount filesystems on boot.
     genfstab -U mnt >> mnt/etc/fstab
@@ -105,7 +127,6 @@ install() {
     cp rootfs/etc/mkinitcpio.conf mnt/etc/mkinitcpio.conf
 
     arch-chroot mnt << EOF
-pacman -S --noconfirm linux
 mkinitcpio -p linux
 bootctl --no-variables --path=/boot install
 chsh -s /usr/bin/fish
@@ -115,21 +136,21 @@ EOF
     # Configure the bootloader entry.
     mkdir -p mnt/boot/loader/entries
     cp rootfs/boot/loader/loader.conf mnt/boot/loader/loader.conf
-    partuuid=`find -L /dev/disk/by-partuuid -samefile ${target}2 | xargs basename`
+    partuuid=`find -L /dev/disk/by-partuuid -samefile $root | xargs basename`
     sed -e "s/XXXX/${partuuid}/" rootfs/boot/loader/entries/arch.conf > mnt/boot/loader/entries/arch.conf
 
     # Set the DNS server.
-    cp rootfs/etc/resolv.conf /mnt/etc/resolv.conf
+    cp rootfs/etc/resolv.conf mnt/etc/resolv.conf
 
     umount mnt/boot
     umount mnt
     rm -r mnt
 }
 
+
 # Check for root user.
 if [[ $EUID -ne 0 ]]; then
-    echo "Run this script as root."
-    exit 1
+    error "run this script as root."
 fi
 
 # Check the arguments.
@@ -137,7 +158,5 @@ if [ -z "$target" ]; then
     usage 1
 fi
 
-# Do the work!
 bootstrap
 install
-
